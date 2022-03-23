e9337b2b197a58d530a8a91547650039a3d8860b
nogiharu <nogiharu@icloud.com>
2022-03-23 13:13:42 +0900

    DataTables追加

--- src/main/resources/static/js/confirmation.js
+++ src/main/resources/static/js/confirmation.js
@@ -1,3 +1,19 @@
+
+$(function() {
+    $("#task-table").dataTable({
+      
+      // DataTablesを日本語化する
+      language: {
+        url: "/webjars/datatables-plugins/i18n/Japanese.json"
+      },
+      lengthMenu: [5, 10, 20],
+      
+      // 各種ボタンを有効化する
+      //dom: "Bfrtip",
+      //buttons: ["excelHtml5", "csvHtml5", "print"]
+    });
+  });
+
 check = (employeeId,name) => {
     let form = document.getElementsByTagName('form')[0]
     if (confirm('この作業は修正できません。本当によろしいですか。')) {
@@ -5,6 +21,14 @@ check = (employeeId,name) => {
         form.method = "get";
         form.submit();
     }
+}
+
+
+update = (employeeId) => {
+  let form = document.getElementsByTagName('form')[0]
+      form.action = "/task/update/" + employeeId;
+      form.method = "get";
+      form.submit();
 }
 
 
