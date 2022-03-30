4e6eaac8b7ec062bbf0e99818a0c081dfdb772cc
nogiharu <nogiharu@example.com>
2022-03-31 08:47:54 +0900

    20220331ローカルストレージほぼ完成

--- src/main/resources/static/js/formAction.js
+++ src/main/resources/static/js/formAction.js
@@ -6,20 +6,14 @@ function addButton() {
     let tr_clone = tr[tr.length - 1].cloneNode(true)
     tr_clone.id = "data" + localStorage.getItem("count")
     parent.appendChild(tr_clone)
-
-
-    /*   var c = document.querySelectorAll("[id^=data]")
-      for (let index = 0; index < c.length; index++) {
-          //console.log(c[index].getAttribute("id"));
-      }
-      let hoge = document.getElementById("data0") */
 }
 function counter() {
     if (!localStorage.getItem("count")) {
         localStorage.setItem("count", 1)
     } else {
         let count = localStorage.getItem("count")
-        localStorage.setItem("count", ++count)
+        localStorage.setItem("count", JSON.stringify(++count))
+        JSON.stringify
     }
 }
 
@@ -27,21 +21,10 @@ function counter() {
 // 「一時保存」押下
 function saveData() {
 
-
-
     let tr = document.getElementsByTagName("tr")
-
     alert(tr.length - 1 + "件一時保存しました！")
     for (let i = 0; i < tr.length; i++) {
-
         let key = document.querySelectorAll("[id^=data]")
-        /* console.log(key[i].children[0].children[0].value);
-        console.log(key[i].children[1].children[0].value);
-        console.log(key[i].children[2].children[0].value);
-        console.log(key[i].children[3].children[0].value);
-        console.log(key[i].children[4].children[0].value);
-        console.log(key[i].children[5].children[0].value); */
-        console.log(key[i].length);
         //let data = []
         //for (let j = 0; j < key[i].children.length-1; j++) {
         //    data = [key[i].children[j].children[0].value,]}
@@ -61,53 +44,30 @@ function saveData() {
 window.onload = function () {
     // ローカルストレージから取得したデータを配列(data)に格納
     if (localStorage.length) {
-        let data = [localStorage.length -1]
+        let data = new Array(localStorage.length)
         for (let i = 0; i < data.length; i++) {
-    
             let jsonData = JSON.parse(localStorage.getItem(localStorage.key(i)))
             data[i] = new Array(jsonData.length)
-            console.log("[A]" +i );
-            for (let j = 0; j < jsonData.length; j++) {
-                console.log("[B]" +i );
+            for (let j = 0; j < data[i].length; j++) {
                 data[i][j] = jsonData[j]
-                console.log("[C]" +i );
             }
         }
+        const newData = data.filter(e => (e.length > 2))
         // 配列(data)のデータの数だけ「tr」タグを生成後、
         // 配列(data)から取得した値を各nameにセット
-        for (let i = 0; i < data.length - 1; i++) {
+        for (let i = 0; i < newData.length; i++) {
             const parent = document.getElementById("parent");
             let tr = document.getElementsByTagName("tr")
             let tr_clone = tr[1].cloneNode(true)
-            if (localStorage.key(i) != "count" && localStorage.key(i) != "data0") {
+            if (localStorage.key(i) != "count") {
                 tr_clone.id = localStorage.key(i)
-                console.log(tr_clone.id);
                 parent.appendChild(tr_clone)
             }
-            //document.getElementsByName("department")[0].value = data[0][0]
-           
-            /* document.getElementsByName("department")[i].value = data[i][0]
-            document.getElementsByName("employeeId")[i].value = data[i][1]
-            document.getElementsByName("area")[i].value = data[i][2]
-            document.getElementsByName("sales")[i].value = data[i][3]
-            document.getElementsByName("customers")[i].value = data[i][4]
-            document.getElementsByName("updateDate")[i].value = data[i][5] */
-            
-            
+            let key = document.querySelectorAll("[id^=data]")
+            for (let j = 0; j < newData[i].length; j++) {
+                key[i].children[j].children[0].value = newData[i][j]
+            }
         }
-        /* document.getElementsByName("department")[0].value = data[1][0]
-            document.getElementsByName("employeeId")[0].value = data[1][1]
-            document.getElementsByName("area")[0].value = data[1][2]
-            document.getElementsByName("sales")[0].value = data[1][3]
-            document.getElementsByName("customers")[0].value = data[1][4]
-            document.getElementsByName("updateDate")[0].value = data[1][5]
-
-            document.getElementsByName("department")[1].value = data[2][0]
-            document.getElementsByName("employeeId")[1].value = data[2][1]
-            document.getElementsByName("area")[1].value = data[2][2]
-            document.getElementsByName("sales")[1].value = data[2][3]
-            document.getElementsByName("customers")[1].value = data[2][4]
-            document.getElementsByName("updateDate")[1].value = data[2][5] */
     }
 }
 
@@ -120,30 +80,20 @@ submit.addEventListener("submit", function () {
 // 「一時保存取り消し」が押下されたらローカルストレージをクリア
 function cancel() {
     alert("取り消しました！")
-    for (let i = 0; i < localStorage.length+1; i++) {
-        if (localStorage.key(i) != "count") {
-            localStorage.removeItem("data" + i)
-        }
+    let count = JSON.parse(localStorage.getItem("count")) 
+    for (let i = 0; i < count+1; i++) {
+        localStorage.removeItem("data" + i)
     }
     location.reload();
 }
+
 
 // 「削除」を押下されたら該当の行を削除
 function deleteTr(obj) {
     const defaultRow = document.getElementsByTagName("tr")
     let tr = obj.parentNode.parentNode
-    //console.log(tr.id);
     if (tr.id != "data0") {
         localStorage.removeItem(tr.id)
-    }
-
-    for (let i = 0; i < localStorage.length; i++) {
-        console.log(localStorage.key(i));
-        if (localStorage.key(i) == tr.id) {
-            localStorage.removeItem(tr.id)
-        }
-    }
-    if (tr.id != "data0") {
         tr.parentNode.deleteRow(tr.sectionRowIndex)
     }
 }
